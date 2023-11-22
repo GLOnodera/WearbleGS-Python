bb5112d36a660803310ea83b57db7888480ecf04
Soraya <sora.haddad123@gmail.com>
2023-11-22 10:49:18 -0300

    funcoes

--- MenuGS.py
+++ MenuGS.py
@@ -29,3 +29,32 @@ def ver_historico(leituras):
         print("\n===== Histórico de Leituras =====")
         for i, leitura in enumerate(leituras, start=1):
             print(f"{i}. Nível de Glicose: {leitura['Nível de Glicose']} - Data e Hora: {leitura['Data e Hora']}")
+
+# Função para calibrar o dispositivo
+def calibrar_dispositivo():
+    print("Calibrando dispositivo...")
+
+# Função para calcular a média das leituras
+def calcular_media(leituras):
+    if not leituras:
+        print("Nenhum dado disponível para calcular a média.")
+    else:
+        media = sum(leitura['Nível de Glicose'] for leitura in leituras) / len(leituras)
+        print(f"A média das leituras é: {media}")
+
+# Função para salvar as leituras em um arquivo
+def salvar_leituras(leituras):
+    with open("leituras.pkl", "wb") as file:
+        pickle.dump(leituras, file)
+    print("Leituras salvas com sucesso!")
+
+# Função para carregar leituras de um arquivo
+def carregar_leituras():
+    try:
+        with open("leituras.pkl", "rb") as file:
+            leituras = pickle.load(file)
+        print("Leituras carregadas com sucesso!")
+        return leituras
+    except FileNotFoundError:
+        return []
+    
